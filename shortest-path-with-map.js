d69018ab326f5bb3af1e8c6fce7aa1960dd20e71
Laura M <laura.merris@googlemail.com>
2018-05-20 21:14:35 +0100

    breadth search ends when end node found

--- shortest-path-with-map.js
+++ shortest-path-with-map.js
@@ -12,29 +12,34 @@ class Graph{
     const visited = new Set();
     const queue = new Queue();
     const path = new Map();
+    let breakout = false;  //immediate breakout if endNode is found
 
     // initialize by adding first vertex to the queue
     queue.enqueue(startNode);
     path.set(startNode, null);
     visited.add(startNode);
 
-    while (queue.items.length > 0){
+
+    while (queue.items.length > 0 & !breakout){
 
       const vertex = queue.dequeue();
 
-      // break condition - destination is reached
-      if (vertex == endNode){
-        break;
-      }
-
       // process adjacent nodes
-      g.AdjList.get(vertex).forEach(function(adj){
+      g.AdjList.get(vertex).some(function(adj){
+
         // process each adjacent node that has not been visited or queued
         if (!visited.has(adj) && queue.items.indexOf(adj) == -1){
+
           // add to queue, mark as visited, record parent metadata
           visited.add(adj);
           queue.enqueue(adj);
           path.set(adj, vertex);
+
+          // breakout if destination node is adjacent
+          if (adj == endNode){
+            breakout = true;
+          }
+          return (adj === endNode);
 
         }
 
@@ -72,43 +77,11 @@ function ShortestPath(map, node){
   return arr.reverse();
 }
 
-// processes all vertices in queue until a desination is reached
-/*
-function BFS(start, destination){
 
-
-  while (queue.items.length > 0){
-
-    const vertex = queue.dequeue();
-
-    // break condition - destination is reached
-    if (vertex == destination){
-      break;
-    }
-
-    // process adjacent nodes
-    g.AdjList.get(vertex).forEach(function(adj){
-      // process each adjacent node that has not been visited or queued
-      if (!visited.has(adj) && queue.items.indexOf(adj) == -1){
-        // add to queue, mark as visited, record parent metadata
-        visited.add(adj);
-        queue.enqueue(adj);
-        pathInfo.set(adj, vertex);
-
-      }
-
-    })
-
-  }
-
-  return path;
-
-}
-*/
 // main
 const g = new Graph();
 const startNode = "A";
-const endNode = "D";
+const endNode = "H";
 
 g.addVertex("A", ["B","C"]);
 g.addVertex("B", ["A", "D", "E", "F"]);
